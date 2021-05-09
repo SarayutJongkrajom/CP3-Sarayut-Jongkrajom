4067446b03447ef1c07755f5ab41d08fcd6092ed
SarayutJongkrajom <SarayutJongkrajom2542@hotmail.com>
2021-05-09 13:59:08 +0700

    Create Exercise4_Sarayut_J.py

---
+++ Exercise4_Sarayut_J.py
@@ -0,0 +1,9 @@
+FE = 90
+GB = 85
+ICS = 99
+CP = 95
+print("============ Your Score =============")
+print("Foundation English               :",FE)
+print("General Business                 :",GB)
+print("Inroduction to Computer Systems  :",ICS)
+print("Computer Programming             :",CP)
