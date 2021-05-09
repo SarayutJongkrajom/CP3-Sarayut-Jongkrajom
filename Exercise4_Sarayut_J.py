dd261fdfa37422a51a5aaa422a1e38adfe073449
SarayutJongkrajom <SarayutJongkrajom2542@hotmail.com>
2021-05-09 14:03:13 +0700

    Update Exercise4_Sarayut_J.py

--- Exercise4_Sarayut_J.py
+++ Exercise4_Sarayut_J.py
@@ -1,7 +1,7 @@
-FE = 90
-GB = 85
-ICS = 99
-CP = 95
+FE = 90.5
+GB = 85.0
+ICS = 99.5
+CP = 95.0
 print("============ Your Score =============")
 print("Foundation English               :",FE)
 print("General Business                 :",GB)
