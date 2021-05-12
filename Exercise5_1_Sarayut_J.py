6ad79180adf14e6db31ed16bf92609bc8c7b3056
SarayutJongkrajom <SarayutJongkrajom2542@hotmail.com>
2021-05-12 12:04:56 +0700

    Create Exercise5_1_Sarayut_J.py

---
+++ Exercise5_1_Sarayut_J.py
@@ -0,0 +1,12 @@
+num0 = int(input("Insert 1 :"))
+num1 = int(input("Insert 2 :"))
+p_add = num0 + num1
+p_sub = num0 - num1
+p_mul = num0 * num1
+p_div = num0 / num1
+print("========== Result Operator ==========")
+print(p_add)
+print(p_sub)
+print(p_mul)
+print(p_div)
+print("=====================================")
