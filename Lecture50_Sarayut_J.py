a4b3fe1ae4f7300689544ba635ba2c4c74003f82
SarayutJongkrajom <SarayutJongkrajom2542@hotmail.com>
2021-05-21 14:51:22 +0700

    Create Lecture50_Sarayut_J.py

---
+++ Lecture50_Sarayut_J.py
@@ -0,0 +1,15 @@
+def inpadd(x,y): # lecture50
+    print(x+y)
+inpadd(1,2)
+
+def inpsub(x,y):
+    print(x-y)
+inpsub(500,400)
+
+def inpmul(x,y):
+    print(x*y)
+inpmul(5,2)
+
+def inpdiv(x,y):
+    print(x/y)
+inpdiv(10,5)
